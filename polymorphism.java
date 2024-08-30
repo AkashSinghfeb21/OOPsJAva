cc8dc782b91df924e7c64fb48ce2f56091c2e1e5
AkashSinghfeb21 <akashsinghfeb21@gmail.com>
2024-08-30 13:06:55 +0530

    polymorphism added

--- polymorphism.java
+++ polymorphism.java
@@ -1,3 +1,24 @@
+//polymorphism is about method ovverriding and overloading
+//Overloading
+class parent{
+    //same name but with different parameters
+    public int add(int a,int b){
+        return a+b;
+    }
+
+    public int add(int a,int b,int c){
+        return a+b+c;
+    }
+} 
 public class polymorphism {
+
+    public static void main(String[]args){ 
+    parent obj = new parent();
+     
+     System.out.println(obj.add(1, 2));
+     
+     System.out.println(obj.add(1, 2,4));
+
+    }
     
 }
