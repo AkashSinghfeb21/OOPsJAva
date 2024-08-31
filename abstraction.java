15b16c895fa6168135aab0d524cabfadde3dfc8b
AkashSinghfeb21 <akashsinghfeb21@gmail.com>
2024-08-31 12:10:02 +0530

    added abstraction polymorphism

--- abstraction.java
+++ abstraction.java
@@ -1,3 +1,49 @@
+//hiding unecesary things and focusing on only what matters
+
+//can achieve abstraction via interface and abstract class
+
+// interface a{
+//     public void sayHello();
+// }
+
+// class Ax implements a{
+//     public void sayHello(){
+//         System.out.println("Hello there");
+//     } 
+// }
+
+abstract class Ax{
+    abstract void startsRunning();
+
+    public void anotherOne(){
+        System.out.println("another One");
+    }
+}
+
+class child extends Ax{
+      public void startsRunning(){
+        System.out.println("Starts Running via inheritance");
+      } 
+}
+
 public class abstraction {
+
+    public static void main(String[]args){
+        // Ax obj = new Ax();
+
+        // obj.sayHello();
+
+        Ax obj = new Ax(){
+            public void startsRunning(){
+                System.out.println("Starts Running via anonymous class");
+            }
+
+        };
+
+        obj.startsRunning();
+        obj.anotherOne();
+
+        new child().startsRunning();
+    }
     
 }
--- polymorphism.java
+++ polymorphism.java
@@ -1,24 +1,50 @@
 //polymorphism is about method ovverriding and overloading
 //Overloading
-class parent{
-    //same name but with different parameters
-    public int add(int a,int b){
-        return a+b;
-    }
+// class parent{
+//     //same name but with different parameters
+//     public int add(int a,int b){
+//         return a+b;
+//     }
 
-    public int add(int a,int b,int c){
-        return a+b+c;
+//     public int add(int a,int b,int c){
+//         return a+b+c;
+//     }
+// } 
+//Overriding
+class parent{
+    public void hello(){
+        System.out.println("Yellow");
     }
-} 
+}
+
+class child extends parent{
+    @Override
+    public void hello(){
+        System.out.println("Override via inheritance");
+    }
+}
+
 public class polymorphism {
 
     public static void main(String[]args){ 
-    parent obj = new parent();
+    // parent obj = new parent();
      
-     System.out.println(obj.add(1, 2));
+    //  System.out.println(obj.add(1, 2));
      
-     System.out.println(obj.add(1, 2,4));
-
-    }
+    //  System.out.println(obj.add(1, 2,4));
     
+    //anonmous class 
+    parent obj = new parent(){
+        @Override
+        public void hello(){
+           System.out.println("Overrided function via anonymous class");  
+        }
+    };  
+
+    child obj2 = new child();
+
+    obj.hello();
+
+    obj2.hello();
+  }    
 }
