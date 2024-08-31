2270fc2200e29fe42d61a6396c90bc5493383a23
AkashSinghfeb21 <akashsinghfeb21@gmail.com>
2024-08-31 12:21:44 +0530

    added encapsulation

--- encapsulation.java
+++ encapsulation.java
@@ -1,3 +1,39 @@
+class parent{
+    private int id;
+    private String name;
+
+    public parent(int id,String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    @Override 
+    public String toString(){
+        return this.id+" "+this.name;
+    }
+}
 public class encapsulation {
+
+    public static void main(String[]args){
+        parent obj = new parent(1, "Akash");
+
+        System.out.println(obj);
+    }
     
 }
